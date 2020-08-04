e125d51c41a019248ff63a964967b01f5271c870
Paul Newby <paulnewby@Pauls-iMac.home>
2020-08-04 19:18:06 +0100

    Updates

---
+++ precache-manifest.e8917d6684830f374a5e7a0f91500b3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e712798ad2ec71a4fec33faeb096075e",
+    "url": "/Markdown-Previewer/index.html"
+  },
+  {
+    "revision": "b79b4e90cfedf6f145c1",
+    "url": "/Markdown-Previewer/static/css/main.f31af40a.chunk.css"
+  },
+  {
+    "revision": "938cd6bf6874423e568a",
+    "url": "/Markdown-Previewer/static/js/2.dcaad67b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Markdown-Previewer/static/js/2.dcaad67b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b79b4e90cfedf6f145c1",
+    "url": "/Markdown-Previewer/static/js/main.d5832061.chunk.js"
+  },
+  {
+    "revision": "b0a69d01708cd88e3db1",
+    "url": "/Markdown-Previewer/static/js/runtime-main.baa23fdd.js"
+  }
+]);
